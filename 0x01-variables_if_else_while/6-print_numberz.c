46dfebd832f70e8a03d3aa78abbbdb628d96c75e
achrafdakir <achraf01dakir@gmail.com>
2023-05-25 16:10:31 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print digit numbers
+ *
+ * Return: sucess
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar("\n");
+
+	return (0);
+}
