056aac582268be2cf1cd4e0aa7cbb2887c4f4081
achrafdakir <achraf01dakir@gmail.com>
2023-05-26 03:45:08 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  all possible combinations of single-digit numbers
+ *
+ * Return: sucess
+ */
+int main(void)
+{
+	int digit = 48;
+
+	while (digit < 58)
+	{
+	putchar(digit);
+	if (digit != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	digit++;
+	}
+
+	return (0);
+}
+
