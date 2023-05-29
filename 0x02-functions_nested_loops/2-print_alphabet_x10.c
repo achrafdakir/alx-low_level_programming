31a98f2013fb750e2064b6a50d49663e857a2452
achrafdakir <achraf01dakir@gmail.com>
2023-05-29 06:13:29 -0700

    print alphabet nfois

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,20 +6,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
+	int i, ch;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch)
 		_putchar('\n');
-		i++;
 	}
 }
