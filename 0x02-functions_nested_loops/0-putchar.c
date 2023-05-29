80ef214b42d78e2d1e341abaa8493b5a74ce2075
achrafdakir <achraf01dakir@gmail.com>
2023-05-29 06:03:33 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * main - create _putchar
+ * main - Write a program that prints _putchar
  *achraf Dakir
  * Return: Success 1 Error -1
 */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+	return (0);
 }
