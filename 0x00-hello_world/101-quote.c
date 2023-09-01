ab346400e87da879f28fa223b6badaa87520a8f8
achrafdakir <achraf01dakir@gmail.com>
2023-09-01 14:59:36 +0100

    quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,12 @@
-#include<stdio.h>
 #include<unistd.h>
+/**
+ * main - the entry point
+ * Return: failed return one
+ */
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
+	write(1, q, 59);
+	return (1);
+}
