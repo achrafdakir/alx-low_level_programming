d3ceb080a366af4236a88a6a0a27b6b3800e5133
achrafdakir <achraf01dakir@gmail.com>
2023-09-07 18:54:08 +0100

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - prints a string on reverse
+ * @s: the string to be reversed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+}
