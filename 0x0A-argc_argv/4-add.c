5bca36f58400f36337f85c729379c4332d23fcb2
achrafdakir <achraf01dakir@gmail.com>
2023-09-13 15:58:22 +0100

    add arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - The entry point
+ * @argc: arguments counter
+ * @argv: arguments vector
+ * Return: succes zero
+ */
+int main(int argc, char **argv)
+{
+	int i, var, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < '0' || *argv[i] > '9')
+		{
+			return (printf("Error\n"), 1);
+		}
+		var = atoi(argv[i]);
+		sum += var;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
