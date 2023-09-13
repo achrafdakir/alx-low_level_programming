557402a00616fe47f615e0e9a57f1f291c5590ce
achrafdakir <achraf01dakir@gmail.com>
2023-09-13 15:04:53 +0100

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always success
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
